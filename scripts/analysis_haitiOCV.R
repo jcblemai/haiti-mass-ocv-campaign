f8e036abac57cf0b269f69c3f2edf36ab76bb69f
Chadi <joseph.lemaitre@epfl.ch>
2018-11-21 17:49:55 +0100

    Work through analysis

--- scripts/analysis_haitiOCV.R
+++ scripts/analysis_haitiOCV.R
@@ -20,6 +20,7 @@ Sys.setenv(TZ='GMT')
 
 # Stochastic model (POMP)
 liks_stoch <- read_csv("results/Haiti_OCVparam_logliks-10-l1.csv")
+liks <- liks_stoch
 
 #colnames(liks_stoch) <- str_replace_all(colnames(liks_stoch), "_", "")
 
@@ -80,180 +81,21 @@ if(doplots) {
 # Likelihood comparison ---------------------------------------------------
 load("data/sirb_cholera_pomped.rda")
 
-# function to compute nesting among models
-isNested <- function(param_vec1, param_vec2) {
-  isnested <-  sum(names(param_vec1)[param_vec1!=0] %>% 
-                     map_dbl(~ sum(!(. %in% names(param_vec2)[param_vec2!=0])))) == 0
-  
-  dof_diff <-  sum(names(param_vec2)[param_vec2!=0] %>% 
-                     map_dbl(~ sum(!(. %in% names(param_vec1)[param_vec1!=0]))))
-  
-  return(c(isnested = isnested, dof_diff = dof_diff, nparam_from = sum(param_vec1!=0), nparam_to = sum(param_vec2!=0)))
-}
 
-
-computeRatioTests <- function(liks, param_est_names, model.type)  {
+# get best likelihood
+best_liks <- liks %>% 
+arrange(desc(loglik)) %>% 
+slice(1)
   
-  # get best likelihoods for each model
-  best_liks <- liks %>% 
-    group_by(model) %>% 
-    arrange(desc(loglik)) %>% 
-    slice(1)
-  
-  # determine model nestings based on paramter values
-  model_nestings <- foreach(mod = iter(best_liks, by = "row"),
-                            .combine = rbind) %do% {
-                              foreach(mod2 = iter(best_liks %>%
-                                                    filter(model != mod$model), by = "row"),
-                                      .combine = rbind) %do% {
-                                        data.frame(model_ref = mod$model,
-                                                   model_comp = mod2$model,
-                                                   as.list(isNested(mod[,param_est_names], mod2[,param_est_names]))) %>%
-                                          mutate(deltaLL = 2* (mod2$loglik - mod$loglik)) %>% 
-                                          as_tibble()
-                                      }
-                            } # %>
-  model_nestings$LLtest = apply(model_nestings[, -c(1:2)] %>% as.matrix, 1, function(x) ifelse(x["deltaLL"] > 0, dchisq(x["deltaLL"], x["dof_diff"]), NA))
-  model_nestings$LLtest2 = apply(model_nestings[, -c(1:2)] %>% as.matrix, 1, function(x) ifelse(x["deltaLL"] > 0, x["deltaLL"] > qchisq(0.95, x["dof_diff"]), NA))
-  
-  model_nestings$type = model.type
-  return(model_nestings)
-}
 
 # Get estimated parameter names
 param_est_names_stoch <- colnames(liks_stoch)  %>% 
-  keep(~ !(. %in% c("loglik", "loglik.se", "model", "type", "k")) & sd(liks_stoch[[.]]) > 1e-3)
-
-param_est_names_deter <- colnames(liks_deter) %>% keep(~  !(. %in% c("eff_v", "MaxE", "RMSE", "model", "type", "loglik")))
-
-# compute model nesting and likelihood ratio tests
-model_nestings <- rbind(computeRatioTests(liks_stoch, param_est_names_stoch, "stochastic"),
-                        computeRatioTests(liks_deter, param_est_names_deter, "deterministic"))
-
-
-# build nesting tree and compute likelihood ratio tests
-
-
-library(tidygraph)
-library(ggraph)
-
-edges <- filter(model_nestings, isnested == 1) %>% 
-  rename(from = model_ref,
-         to = model_comp) %>% 
-  mutate(from = as.numeric(from),
-         from = ifelse(type == "stochastic", from + 8, from),
-         to = as.numeric(to),
-         to = ifelse(type == "stochastic", to + 8, to))
-
-best_liks <-rbind(liks_stoch %>% select(loglik, loglik.se, model, type),
-                  liks_deter %>% select(loglik, model, type) %>% mutate(loglik.se = 0)) %>% 
-  group_by(type, model) %>% 
-  arrange(desc(loglik)) %>% 
-  slice(1)
-
-nodes <-  best_liks %>% 
-  ungroup %>% 
-  mutate(id = as.numeric(model)) %>% 
-  select(id, model, loglik, loglik.se, type) %>%
-  mutate(id = ifelse(type == "stochastic", id +8, id))
-
-nodes$x <- rep(c(1, 1.75, .75, 1.25, 1, 1.5, 2, 1.75), times = 2)
-
-nodes$nparam <- foreach(r = iter(best_liks, by = "row"), .combine = c) %do% {
-  if(r[["type"]] == "deterministic") {
-    x <- filter(liks_deter, loglik == r$loglik, model == r$model) %>%  slice(1)
-    sum(x[param_est_names_deter]>0)
-  } else {
-    x <- filter(liks_stoch, loglik == r$loglik, model == r$model) %>% slice(1)
-    sum(x[param_est_names_stoch]>0)
-  }
-}
-
-
-nodes$y <-nodes$nparam
-model_network <- tbl_graph(edges = edges,
-                           nodes = nodes,
-                           directed = T) 
-
-
-# plot the plot
-pg_labels <- tribble(
-  ~type, ~label,
-  "deterministic", "A",
-  "stochastic", "B"
-) %>% 
-  mutate(x = min(nodes$x), y = max(nodes$y) + 0.5)
-
-deltax <- 0.1
-
-p.graph <- ggraph(model_network) +
-  # geom_node_point(aes(x = x, y = y, size = nparam, color = loglik)) + 
-  geom_edge_link(aes(color = LLtest2 , linetype = !LLtest2), width = .8) +
-  geom_node_label(aes(label = str_c(model, sprintf("%.2f",loglik), sep = "\n")), size = 3.2) +
-  scale_edge_color_manual(values = c("grey", "black"), guide = "none") +
-  scale_edge_linetype(guide = "none") +
-  scale_y_continuous(breaks = seq(7, 13), limits = c(6.8, 13.5)) +
-  scale_x_continuous(limits = c(min(nodes$x) - deltax, max(nodes$x) + deltax)) +
-  geom_text(data = pg_labels, aes(x = x, y = y, label = label), size = 7) +
-  facet_wrap(~type) +
-  theme_graph() +
-  labs(x = "" , y = "# of paramters") +
-  theme(panel.grid.major.y = element_line(color = "#DED8D8"),
-        axis.text.y = element_text(size = 10),
-        axis.title = element_text(size = 12),
-        strip.text = element_blank()) 
-
-p.graph
-ggsave(p.graph, file = "results/figures/model_nesting_LLratio_test.png", width = 9, height = 5, dpi = 300)
-
+  keep(~ !(. %in% c("loglik", "loglik.se", "type", "k")) & sd(liks_stoch[[.]]) > 1e-3)
 
 
 # Compare outputs ---------------------------------------------------------
 
-# DETERMINISTIC MODEL
-ode_cases <- read_csv("data/ODE_dates.csv", col_names = F) %>% 
-  set_colnames("date") %>% 
-  mutate(date = date - 1) %>% 
-  bind_cols(read_csv("results/ODE_trajectory_MN.csv", col_names = F)[101,] %>% 
-              unlist() %>% 
-              tibble(cases = .)) 
-
-
 doMC::registerDoMC(8)
-
-sim_deter_quantiles <- foreach(model = c("MN", "ME", "MEC"), 
-                               .combine = rbind) %do% {
-                                 # load trajectories
-                                 ode_out <- read_csv(str_c("results/ODE_trajectory_", model ,".csv"), col_names = F)
-                                 # remove last row containing the data
-                                 ode_out <- ode_out[-nrow(ode_out), ]
-                                 
-                                 # run simulations from the deterministic models
-                                 nsim_deter <- 100
-                                 
-                                 sim_deter <- foreach(it = iter(ode_out, by = "row"), 
-                                                      ic = icount(),
-                                                      .combine = cbind) %dopar% {
-                                                        matrix(rpois(nrow(ode_cases) * nsim_deter, unlist(it)), ncol = nsim_deter) %>% 
-                                                          as.data.frame() %>% 
-                                                          set_colnames(str_c("sim", ic, "_", seq(nsim_deter)))
-                                                      } %>% 
-                                   as_tibble()
-                                 
-                                 foreach(it = iter(sim_deter, by = "row"), 
-                                         .combine = rbind) %dopar% {
-                                           x <- unlist(it)
-                                           tibble(
-                                             q05 = quantile(x, 0.025, na.rm = T),
-                                             mean = mean(x, na.rm = T),
-                                             q50 = quantile(x, 0.5, na.rm = T),
-                                             q95 = quantile(x, 0.975, na.rm = T)
-                                           )
-                                         } %>% 
-                                   bind_cols(ode_cases) %>% 
-                                   mutate(model = model)
-                               } %>% 
-  mutate(type = "deterministic")
 
 # STOCHASTIC MODEL
 # Auxillary functions
@@ -292,27 +134,20 @@ simulatePOMP <- function(pmp, params, nsim, seed = 199919L) {
 
 
 
-# get MLE paramter sets for the SIRB and SIRB-Exposure models for final plot 
+# get MLE paramter sets 
 best_param <- best_liks %>% 
-  filter(type == "stochastic", model %in% c("MN", "ME", "MEC")) %>% 
   arrange(desc(loglik)) %>% 
   ungroup %>% 
-  left_join(liks_stoch) %>% 
-  select(model, one_of(names(coef(sirb_cholera)))) 
+  left_join(liks_stoch)
 
-# This is to avoid the problem of the rainfall normalization
-rain <- read_csv("data/rainfall_data.csv") %>% 
-  mutate(date = as.Date(date, format = "%d-%b-%y"),
-         time = dateToYears(date),
-         rain_std = rain/max(rain))
-
+# How to change covariate if necessary: Javier recrated a pmp object with a covariate he can change
 # re-create pomp object for simulations
-sirb_cholera_maxrain <- pomp(sirb_cholera,
-                             covar = rain %>%
-                               filter(time >= min(sirb_cholera@tcovar) & time <= max(sirb_cholera@tcovar)) %>% 
-                               select(time, rain_std) %>%
-                               rename(rain = rain_std),
-                             tcovar = "time")
+# sirb_cholera_maxrain <- pomp(sirb_cholera,
+#                              covar = rain %>%
+#                                filter(time >= min(sirb_cholera@tcovar) & time <= max(sirb_cholera@tcovar)) %>% 
+#                                select(time, rain_std) %>%
+#                                rename(rain = rain_std),
+#                              tcovar = "time")
 
 # run simulations for each model
 sim_stochastic <- foreach(r = iter(best_param, by = "row"), 
