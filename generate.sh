7f48d91d33d8aa3c8f3f2a3e39ad2122a4694d26
chadi <joseph.lemaitre@epfl.ch>
2019-04-08 17:32:49 +0200

    state for deliverable

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #runlvl=$1
 output_dir="output_16-04-init"
 
-for dept in Artibonite; do #Centre Ouest Nord-Ouest Nord Sud  Nippes Nord-Est Sud-Est Grande_Anse; do
+for dept in Artibonite  Nord-Ouest Nord Nippes Centre Ouest Nord-Ouest Nord Sud  Nippes Nord-Est Sud-Est Grande_Anse; do
     rm -R output/$dept/
     mkdir output/$dept;
     Rscript scripts/pomp_cholera_haitiOCV.R $dept;
